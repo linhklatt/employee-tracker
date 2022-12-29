49abf0716dd1be5c630ad9e2c47215bf71f41ae7
linhklatt <112294288+linhklatt@users.noreply.github.com>
2022-12-29 15:11:02 -0800

    modify schema.sql, added more argument into role and employee table

--- db/schema.sql
+++ db/schema.sql
@@ -5,35 +5,30 @@ USE employee_db;
 
 -- department table
 CREATE TABLE department(
-
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
     name VARCHAR(30) UNIQUE NOT NULL
 );
 
 -- role table
-CREATE TABLE role(
-
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-    title VARCHAR(30) UNIQUE NOT NULL,
-
-    salary DECIMAL UNIQUE NOT NULL,
-
-    department_id INT UNSIGNED NOT NULL,
-    
-
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL UNIQUE,
+  salary DECIMAL NOT NULL UNIQUE,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
 );
 -- employee table
-CREATE TABLE employee(
-
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-    first_name VARCHAR(30),
-
-    last_name VARCHAR(30),
-
-    role_id INT,
-
-    manager_id INT
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED NOT NULL,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
+
+USE employee_db;
