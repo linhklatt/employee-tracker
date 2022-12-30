c551402b2c9de23f9361e0d836594d56c9b479d4
linhklatt <112294288+linhklatt@users.noreply.github.com>
2022-12-29 16:44:20 -0800

    created employee, role and department questions

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employee_db database.`)
 );
-
+/***********************************************************/
 // show the title for the employee tracker table
 console.log("------------EMPLOYEE MANAGER------------");
-
+/***********************************************************/
 // the app questions
 const startQuestion = [
   {
@@ -47,7 +47,7 @@ const mainQuestion = [
     ],
   },
 ];
-
+/***********************************************************/
 // Function to view all department table
 function viewAllDepartments() {
   db.query("SELECT * FROM department"),
@@ -70,3 +70,83 @@ function viewAllEmployees() {
       console.table(results);
     };
 }
+/***********************************************************/
+// Employee Questions
+let newEmployee = [];
+
+const employeeQuestions = [
+  {
+    type: "input",
+    message: "What is the employee's first name?",
+    name: "firstName",
+  },
+  {
+    type: "input",
+    message: "What is the employee's last name?",
+    name: "lastName",
+  },
+
+  {
+    type: "list",
+    message: "What is the employee's role?",
+    name: "role",
+    choices: [
+      "Sales Lead",
+      "Sales Person",
+      "Lead Engineer",
+      "Software Engineer",
+      "Account Manager",
+      "Accountant",
+      "Legal Team Lead",
+      "Lawyer",
+    ],
+  },
+  {
+    type: "list",
+    message: "Who is the employee's manager?",
+    name: "manager",
+    choices: [
+      "John Doe",
+      "Mike Chan",
+      "Ashley Rodriquez",
+      "Kevin Tupik",
+      "Kunal Singh",
+      "Malia Brown",
+      "Sarah Lourd",
+      "Tom Allen",
+    ],
+  },
+];
+/***********************************************************/
+// Role Questions
+let newRole = [];
+
+const roleQuestions = [
+  {
+    type: "input",
+    message: "What is the name of the role?",
+    name: "roleName",
+  },
+  {
+    type: "input",
+    message: "What is the salary of the role?",
+    name: "roleSalary",
+  },
+
+  {
+    type: "list",
+    message: "What department does the role belong to?",
+    name: "roleDepartment",
+    choices: ["Sales", "Engineering", "Finance", "Legal"],
+  },
+];
+/***********************************************************/
+// Department Questions
+let newDepartment = [];
+const departmentQuestions = [
+  {
+    type: "input",
+    message: "What is the name of the department?",
+    name: "departmentName",
+  },
+];
